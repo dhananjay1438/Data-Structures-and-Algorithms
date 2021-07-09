06a1411e81de92b0c0c137a2eba93adc1e934801
Dhananjay Panage <dhananjaypanage11@gmail.com>
2021-07-09 13:19:15 +0530

    Checks if two BST are same

--- trees/binary-trees/check_if_both_trees_are_equal.cpp
+++ trees/binary-trees/check_if_both_trees_are_equal.cpp
@@ -1,5 +1,80 @@
+#include "../headers/binary_search_tree.hh"
 #include <iostream>
+#include <queue>
+#include <stack>
 
-class BinarySearchTree {};
+struct node {
+  node *left;
+  int data;
+  node *right;
+  node(int);
+};
 
-int main(void) { return 0; }
+node::node(int data) : left(nullptr), data(data), right(nullptr){};
+
+class MyBST : public BST<node> {
+  static int _compare(node *, node *);
+public:
+  friend int compare(MyBST, MyBST);
+};
+
+int MyBST::_compare(node *first, node *second) {
+
+  std::stack<node *> fstack;
+  std::queue<node *> fqueue;
+  std::stack<node *> sstack;
+  std::queue<node *> squeue;
+
+  while (first || !fstack.empty()) {
+    while (first) {
+      fstack.push(first);
+      first = first->left;
+    }
+    first = fstack.top();
+    fstack.pop();
+    fqueue.push(first);
+
+    first = first->right;
+  }
+
+  while (second || !sstack.empty()) {
+    while (second) {
+      sstack.push(second);
+      second = second->left;
+    }
+    second = sstack.top();
+    sstack.pop();
+    squeue.push(second);
+
+    second = second->right;
+  }
+
+  if (fqueue.size() != squeue.size()) {
+    return -2;
+  }
+  while (!fqueue.empty()) {
+
+    if (fqueue.front()->data != squeue.front()->data) {
+      return -1;
+    }
+    fqueue.pop();
+    squeue.pop();
+  }
+  return 1;
+};
+
+int compare(MyBST first, MyBST second) {
+	return MyBST::_compare(first.root, second.root);
+}
+
+int main(void) {
+  MyBST first, second;
+
+  first.insert(50);
+
+  second.insert(50);
+
+  std::cout << compare(first, second);
+
+  return 0;
+}
